dae30335e35a6280ab84b1a721d96c9254f4cdcf
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-13 21:30:45 +0100

    deleted files from master branch

--- models/review.py
+++
@@ -1,8 +0,0 @@
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """Review class that inherits from BaseModel."""
-    place_id: str = ""
-    user_id: str = ""
-    text: str = ""
